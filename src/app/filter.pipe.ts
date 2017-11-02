e350291f494c624d27e08d416b5874400b810e7b
nunosoares27 <nuno.soares@ua.pt>
2017-11-02 12:20:47 +0000

    pesquisa por primeiro nome a funcionar mas apenas se o primeiro nome tiver completo

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,14 +1,34 @@
 import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
-  name: 'filter'
+  name: 'filterByName'
 })
 export class FilterPipe implements PipeTransform {
-  transform(items: any[], searchText: string): any[] {
-    if(!items) return [];
-    if(!searchText) return items;
-searchText = searchText.toLowerCase();
-return items.filter( it => {
-      return it.toLowerCase().includes(searchText);
-    });
-   }
+
+
+//   transform(items: any[], searchText: string): any[] {
+//     if(!items) return [];
+//     if(!searchText) return items;
+// searchText = searchText.toLowerCase();
+// return items.filter( it => {
+//       return it.toLowerCase().includes(searchText);
+//     });
+//    }
+
+
+ 
+  transform(input: any[], searchText: any[]){
+    var output: any[] = [];
+    if(!input) return [];
+    if(!searchText) return input;
+    searchText = searchText;
+    for (var i = 0; i < input.length; i++) {
+      if (input[i].first_name === searchText) {
+        output.push(input[i]);
+      }
+    }
+    return output;
+  }
+
+
+
 }
